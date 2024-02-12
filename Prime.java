cc1615c4be7af430e0220c5e02d6836e06187ec8
anugraha2011 <anugrahamolbmk@gmail.com>
2024-02-12 17:59:27 +0530

    Is Prime

---
+++ Prime.java
@@ -0,0 +1,13 @@
+class Test{
+	public static boolean isPrime(int num)
+		int count=0;
+		for(int i=1;i<=num;i++){
+			if(num%i==0){
+				count++;
+			}
+		}if(count==2){
+			return true;
+		}else
+			return false;
+	}
+}
